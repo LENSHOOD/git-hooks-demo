3cfcdf69866b70d5a7883c8133db20822961e73c
lenshood <zhangxh_job@sina.com>
2019-06-02 01:08:59 +0800

    chore: add checkstyle

---
+++ src/main/java/zxh/demo/git/hooks/GitHooksMain.java
@@ -0,0 +1,12 @@
+package zxh.demo.git.hooks;
+
+/**
+ * 
+ * @author zhangxuhai
+ * @date 2019-06-01
+*/
+public class GitHooksMain {
+    public static void main(String[] args) {
+        System.out.println("Git Hooks Demo");
+    }
+}
